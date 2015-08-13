61c470335ba475e26764ae816bdf8fc043a0e6e7
gulaschskanone <christoph.becker@uni-passau.de>
2015-08-13 11:12:52 +0200

    added comments

--- static/js/main.js
+++ static/js/main.js
@@ -3,26 +3,34 @@ var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 
 
 exports.postAceInit = function (hook_name, args, cb) {
-	// delete $.fn.farbtastic;
-	// $(".farbtastic").remove();
+	/**
+	 * remove farbtastic
+	 */
+	// delete $.fn.farbtastic; // doesn't work
+	// very dirty:
 	$("#mycolorpicker").remove();
 	$("#myuser").prepend( "<div id='colorpicker'></div>" );
+	// TODO: improve the way to remove farbtastic
 	
-	
+	/**
+	 * bind brightColorPicker plugin
+	 */
 	$('#colorpicker').brightColorPicker({
 		'brightness' : 0.25,
+		// TODO: put brightness level into settings.json
 		'callback' : function (color) {
 			pad.myUserInfo.colorId = color;
 			pad.notifyChangeColor(color);
+			// paduserlist.renderMyUserInfo(); // doesn't work
+			// dirty: copied rows from paduserlist.renderMyUserInfo()
 			$("#myswatch").css({'background-color': pad.myUserInfo.colorId});
-		    
 			if (browser.msie && parseInt(browser.version) <= 8) {
 				$("li[data-key=showusers] > a").css({'box-shadow': 'inset 0 0 30px ' + pad.myUserInfo.colorId,'background-color': pad.myUserInfo.colorId});
 		    }
-		    else
-		    {
+		    else {
 		       $("li[data-key=showusers] > a").css({'box-shadow': 'inset 0 0 30px ' + pad.myUserInfo.colorId});
 		    }	
+			// TODO: run paduserlist.renderMyUserInfo()
 		}
 	});
 	
